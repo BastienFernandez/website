3bb52f04800b8fe7517cb1d4220a8c55044b92c6
Bastien Fernandez <fernandez.bastien.mail@gmail.com>
2025-11-10 20:24:07 +0100

    Setup app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { ProfilePage } from './pages/profile-page/profile-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomePage,
+    },
+    {
+        path: 'profile',
+        component: ProfilePage,
+    }
+];
